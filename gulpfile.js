e6bc40777bb9b540ead3b647c991d0f1345a611e
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-12-12 18:56:19 -0500

    npx gulp dev and npm run dev

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const { src, dest } = require('gulp');
+const { src, dest, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 function css(done) {
@@ -13,4 +13,12 @@ function css(done) {
   done(); // callback que le avisa a gulp que ya terminé
 }
 
+function dev(done) {
+  watch('src/scss/app.scss', css)
+
+
+  done();
+}
+
 exports.css = css;
+exports.dev = dev;
