3adade17156f62e8d35fd26399bcb3ffa7d1d3f3
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-12-12 20:04:16 -0500

    no hace nada

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,17 @@ const sass = require('gulp-sass')(require('sass'));
 
 function css(done) {
   // Identificar el archivo de SASS
+  // Compilarlo
+  // Guardarlo en un archivo CSS
   src('src/scss/app.scss')
     .pipe(sass())
     .pipe(dest('build/css'));
 
-  // Compilarlo
-
-  // Guardarlo en un archivo CSS
   done(); // callback que le avisa a gulp que ya terminé
 }
 
 function dev(done) {
   watch('src/scss/app.scss', css)
-
 
   done();
 }
