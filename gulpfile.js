d8985fdb11036e7c042f1a4dc018a327bb05599d
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-12-20 09:00:22 -0500

    galeria

--- gulpfile.js
+++ gulpfile.js
@@ -48,15 +48,25 @@ function versionAvif(done) {
   done(); // callback que le avisa a gulp que ya terminé
 }
 
+// Javascript
+function javascript(done) {
+  src('src/js/**/*.js')
+    .pipe(dest('build/js'));
+
+  done();
+}
+
 function dev(done) {
   watch('src/scss/**/*.scss', css);
+  watch('src/js/**/*.js', javascript);
 
   done();
 }
 
 exports.css = css;
+exports.js = javascript;
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
 exports.versionAvif = versionAvif;
-exports.dev = parallel(imagenes, versionWebp, versionAvif, dev);
+exports.dev = parallel(imagenes, versionWebp, versionAvif, javascript, dev);
 dev;
---
+++ src/js/app.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function () {
+  iniciarApp();
+});
+
+function iniciarApp() {
+  crearGaleria();
+}
+
+function crearGaleria() {
+  const galeria = document.querySelector('.galeria-imagenes');
+
+  for (let i = 1; i <= 12; i++) {
+    const imagen = document.createElement('picture');
+
+    imagen.innerHTML = `
+        <source srcset="build/img/thumb/${i}.avif" type="image/avif" />
+        <source srcset="build/img/thumb/${i}.webp" type="image/webp" />
+        <img
+          loading="lazy"
+          width="200"
+          height="300"
+          src="build/img/thumb/${i}.jpg"
+          alt="Imagen Geleria ${i}"
+        />
+      `;
+
+      galeria.appendChild(imagen);
+  }
+}
