7a86ec81cf0e62884df33e159369c01b76dff9f0
Elis Antonio Perez <elisperezmusic@gmail.com>
2023-01-12 11:25:03 -0500

    scrollIntoView

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,19 @@ document.addEventListener('DOMContentLoaded', function () {
 
 function iniciarApp() {
   crearGaleria();
+  scrollNav();
+}
+
+function scrollNav() {
+  const enlaces = document.querySelectorAll('.navegacion-principal a');
+  enlaces.forEach(enlace => {
+    enlace.addEventListener('click', e => {
+      e.preventDefault();
+      const seccionScroll = e.target.attributes.href.value;
+      const seccion = document.querySelector(seccionScroll);
+      seccion.scrollIntoView({ behavior: 'smooth' });
+    });
+  });
 }
 
 function crearGaleria() {
