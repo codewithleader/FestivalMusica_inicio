e339546715c95ad3b65c3f152939a8572a5c360c
Elis Antonio Perez <elisperezmusic@gmail.com>
2023-01-12 14:04:43 -0500

    build

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@ const { src, dest, watch, parallel } = require('gulp');
 // CSS
 const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
 
 // Imagenes
 const cache = require('gulp-cache');
@@ -10,11 +14,20 @@ const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
 const avif = require('gulp-avif');
 
+// Javascript
+const terser = require('gulp-terser-js');
+
 function css(done) {
   // Identificar el archivo de SASS
   // Compilarlo
   // Guardarlo en un archivo CSS
-  src('src/scss/**/*.scss').pipe(plumber()).pipe(sass()).pipe(dest('build/css'));
+  src('src/scss/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest('build/css'));
 
   done(); // callback que le avisa a gulp que ya terminé
 }
@@ -51,6 +64,9 @@ function versionAvif(done) {
 // Javascript
 function javascript(done) {
   src('src/js/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(terser())
+    .pipe(sourcemaps.write('.'))
     .pipe(dest('build/js'));
 
   done();
